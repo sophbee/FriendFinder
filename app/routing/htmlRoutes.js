81a7d22a2c557be54e1541444a2316a0ca137b75
Sophia Butterworth <sophiabutterworth@gmail.com>
2018-05-01 18:50:06 -0400

    Adding a route for CSS

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,11 +9,11 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
+   app.get("styles/styles.css", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/styles/styles.css"));
+  });
+   
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-
-  app.get("styles/styles.css", function(req, res) {
-    res.sendFile(path.join(__dirname, "/styles/styles.css"));
   });
 };
