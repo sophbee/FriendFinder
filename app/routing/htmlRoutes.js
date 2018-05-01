803fba84e4075ba4b0ebdd1eb030b941602e4b8a
Sophia Butterworth <sophiabutterworth@gmail.com>
2018-05-01 18:18:02 -0400

    Added static styles.css path

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -12,4 +12,8 @@ module.exports = function(app) {
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
+
+  app.get("/styles/styles.css", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/styles/styles.css"));
+  });
 };
