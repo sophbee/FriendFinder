5c9c49127b2d0fc8fac25dc96e4e2b6853a6699b
Sophia Butterworth <sophiabutterworth@gmail.com>
2018-05-01 20:17:49 -0400

    Adding static route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -13,6 +13,7 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/styles/styles.css"));
   });
    
+  app.static('../public');
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
