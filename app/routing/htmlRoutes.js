c9360f681082c2d2cd6549875530a72a92937970
Sophia Butterworth <sophiabutterworth@gmail.com>
2018-05-01 20:34:58 -0400

    Another fix for static images

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,20 +1,21 @@
 var path = require("path");
 
-module.exports = function(app) {
-  app.get("/", function(req, res) {
+module.exports = function (app) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
-  app.get("/survey", function(req, res) {
+  app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
-   app.get("styles/styles.css", function(req, res) {
+  app.get("styles/styles.css", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/styles/styles.css"));
   });
-   
-  app.static('../public');
-  app.get("*", function(req, res) {
+
+  app.use(express.static("public"));
+
+  app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
